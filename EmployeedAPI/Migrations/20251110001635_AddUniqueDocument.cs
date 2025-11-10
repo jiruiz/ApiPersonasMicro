8673c7d6d246fec07d54a46bc21fc39d387f4b1d
Pablo Calvo <167559815+pc250282_ncrvoyix@users.noreply.github.com>
2025-11-09 21:19:15 -0300

    add documenet field

---
+++ EmployeedAPI/Migrations/20251110001635_AddUniqueDocument.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeedAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueDocument : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
